68e6464c8818319f74bef3930e9245198073bfaf
tyt <2240484340@qq.com>
2024-05-30 10:45:35 +0800

    test for static lib

---
+++ lib/static/lib/include/math.h
@@ -0,0 +1,4 @@
+extern int errno;
+
+int add(const int x, const int y);
+int div(const int x, const int y);
---
+++ lib/static/main.c
@@ -0,0 +1,7 @@
+#include"math.h"
+#include<stdio.h>
+
+int main(){
+	printf("1 + 2 = %d\n", add(1, 2));
+	return 0;
+}
