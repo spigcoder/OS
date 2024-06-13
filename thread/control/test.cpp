f69aa6910bd683d402fb252e1b529a61d1711fc8
tyt <2240484340@qq.com>
2024-06-13 09:28:37 +0800

    test for multiThread

--- thread/control/test.cpp
+++ thread/control/test.cpp
@@ -1,30 +1,59 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+#include <vector>
 using namespace std;
-int i = 0;
+
+const int NUM = 4;
 
 //thread()
-// void *ThreadRoutine(void *args){
-//     int num = 10000;
-//     while(num--){
-//         i++;
-//     }
-//     return nullptr;
-// }
+void *ThreadRoutine(void *args){
+    int num = 10000;
+    while(num--){
+        i++;
+    }
+    return nullptr;
+}
 
-// void TestThread(){
-//     pthread_t tid;
-//     pthread_create(&tid, nullptr, ThreadRoutine, nullptr);
-//     int num = 100000;
-//     while(num--){
-//         i++;
-//     }
-//     pthread_join(tid, nullptr);
-// }
+void TestThread(){
+    pthread_t tid;
+    pthread_create(&tid, nullptr, ThreadRoutine, nullptr);
+    int num = 100000;
+    while(num--){
+        i++;
+    }
+    pthread_join(tid, nullptr);
+}
+
+//测试多线程并证明每个线程有自己独立的栈结构
+void *TestMultiThread(void *args){
+    int i = 0;
+    int num = 10;
+    while(num--){
+        //这里可以证明每个线程都有自己独立的栈结构，但是像这样的打印也是不安全的
+        //因为他会访问共享资源：显示器，这就会出现乱打印的现象
+        cout << "i: " << i << " &i: " << &i << endl;
+        i++;
+        sleep(1);
+    }
+    return nullptr;
+}
+
+void TestMThread(){
+    vector<pthread_t> tids;
+    for(int i = 0; i < NUM; ++i){
+        pthread_t id;
+        pthread_create(&id, nullptr, TestMultiThread, nullptr);
+        tids.push_back(id);
+    }
+
+    for(int i = 0; i < NUM; ++i){
+        pthread_join(tids[i], nullptr);
+    }
+}
 
 int main(){
     // TestThread();
-    cout << i << endl;
+    // TestMThread();
     return 0;
 }
